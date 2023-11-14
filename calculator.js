2bf3c4f33f0c489cb3690d65c00bb8638fda5802
unknown <brosjayme@gmail.com>
2023-11-14 10:56:48 +0100

    added body-parser to the serverfile

--- calculator.js
+++ calculator.js
@@ -12,7 +12,17 @@ app.post("/", function (req, res) {
   var num2 = Number(req.body.num2);
 
   var result = num1 + num2;
-  res.send("the result of the calculation is" + result);
+  res.send("the result of the calculation is: " + result);
+});
+
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+app.post("/bmicalculator", function (req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var bmi = weight / (height * height);
+  res.send("your BMI: " + bmi);
 });
 
 app.listen(3000, function () {
